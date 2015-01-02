279c18f6c71f61523bd489a6b60796d53c2043b7
Johannes Brodwall <johannes@brandmaster.com>
2015-01-03 00:25:54 +0100

    Initial version

---
+++ lib/bin/backup-db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+DATABASE=$1
+TARGET_DIR=/shared/db/$DATABASE
+TARGET_USER=$USER
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+sudo rm /tmp/$DATABASE.*
+
+sudo -u postgres pg_dump --format=c --file=/tmp/$DATABASE.dmp $DATABASE
+sudo -u postgres pg_dump --format=c --schema-only --file=/tmp/$DATABASE.schema.dmp $DATABASE
+sudo -u postgres pg_dump --format=c --data-only --table=status_codes --table=countries --file=/tmp/$DATABASE.seed.dmp $DATABASE
+sudo chown $TARGET_USER /tmp/$DATABASE.*
+bzip2 /tmp/$DATABASE.*dmp
+
+mkdir -p $TARGET_DIR
+cp /tmp/$DATABASE.dmp.bz2 $TARGET_DIR/$DATABASE-latest.dmp.bz2
+cp /tmp/$DATABASE.dmp.bz2 $TARGET_DIR/$DATABASE-$(date +hr%H).dmp.bz2
+cp /tmp/$DATABASE.dmp.bz2 $TARGET_DIR/$DATABASE-$(date +%a).dmp.bz2
+cp /tmp/$DATABASE.dmp.bz2 $TARGET_DIR/$DATABASE-$(date +%Y-m%m).dmp.bz2
+
+cp /tmp/$DATABASE.schema.dmp.bz2 $TARGET_DIR/$DATABASE.schema-latest.dmp.bz2
+cp /tmp/$DATABASE.seed.dmp.bz2 $TARGET_DIR/$DATABASE.seed-latest.dmp.bz2
